c3c28fc03dee27dadaec9aae5e9c18fc314a88c9
Olia_Antipenko <olia95shash@gmail.com>
2020-08-22 01:10:02 +0300

    Updates

---
+++ precache-manifest.ec9644aca61374a5f8ee8bb50686aebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de43a8b3ef3aac08d3e2036296c371f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "eac3a4f98e4711740efc",
+    "url": "/goit-react-hw-01-components/static/css/main.948681c3.chunk.css"
+  },
+  {
+    "revision": "8702e87f2de9d9d43916",
+    "url": "/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.f39d56b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac3a4f98e4711740efc",
+    "url": "/goit-react-hw-01-components/static/js/main.11a33e99.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
